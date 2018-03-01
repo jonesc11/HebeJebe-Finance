48188de1e5965e5bfaeca8930998c98bb8e00980
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-02-28 19:27:51 -0500

    Added empty junit test cases

---
+++ src/finance/CreateTransactionActionObject.java
@@ -0,0 +1,14 @@
+package finance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreateTransactionActionObject {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
