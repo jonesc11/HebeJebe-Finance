382a6c55de7f6ed348fe92c71b26ef5388632e38
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-03-22 18:25:40 -0400

    added body to the request

--- web-end/public/resources/js/script.js
+++ web-end/public/resources/js/script.js
@@ -2,10 +2,19 @@ $(document).ready (function () {
 	var app = angular.module("MyApp", []);
 
 	app.controller("PostsCtrl", function($scope, $http) {
-		$http.get('request/transaction.json').
+		$http({
+  			method: 'GET',
+  			url: 'request/transaction.json'),
+  			data: {
+  				"Limit": 30,
+  				"ResourceIdentifier": "tr",
+  				"GetFrom": "User",
+  				"TransactionType": null, 
+  				"Category": null,
+  				"NextToken": null 
+			}).
 			success(function(data, status, headers, config) {
 				$scope.transactions = data;
-
 				}).
 			error(function(data, status, headers, config) {
 				// log error
