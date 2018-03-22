f112db963a777b2bb3d2d79cb5095a7a2eb98860
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-03-22 18:09:48 -0400

    First commit of angular http get call

--- web-end/public/resources/js/script.js
+++ web-end/public/resources/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready (function () {
-  
+	var app = angular.module("MyApp", []);
+
+	app.controller("PostsCtrl", function($scope, $http) {
+		$http.get('request/transaction.json').
+			success(function(data, status, headers, config) {
+				$scope.transactions = data;
+
+				}).
+			error(function(data, status, headers, config) {
+				// log error
+		});
+	});  
 });
