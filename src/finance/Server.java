019b7c48a1c1abc7f9cb1ea9927a56ea4bc4b5a1
Hannah Deen <hannahdeen@folsom-wl-234.dynamic2.rpi.edu>
2018-03-23 15:32:16 -0400

    adding server, client, and java serilization packages

---
+++ src/finance/Server.java
@@ -0,0 +1,83 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+/*
+Server is used to receive messages from over processes
+ */
+
+public class Server {
+
+    private int id;
+
+    public server(int id)
+    {
+        this.id = id;   
+    }
+
+    public void run()
+    {
+        try
+        {
+            int socketNumber = 9100 + id;
+            ServerSocket listener = new ServerSocket(socketNumber);            
+            try 
+            {
+                while (true) 
+                {
+                    new Handler(listener.accept()).start();
+                }
+            } 
+            finally 
+            {
+                listener.close();
+            }
+        }
+        catch (IOException e) 
+        {
+            System.out.println(e);
+        }
+    }
+
+    public class Handler extends Thread
+    {
+        private BufferedReader in;
+        private PrintWriter out;
+        private Socket socket;
+        
+        public Handler(Socket socket) 
+        {
+            this.socket = socket;
+        }
+
+        public void run() 
+        {
+            try 
+            {
+                String clientMsg;
+                String json = "";
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+                while((clientMsg = in.readLine()) != null)
+                {
+                    writer.append(clientMsg);
+                    json += clientMsg;
+                }
+                writer.close();
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                Message msg = mapper.readValue(json, Message.class);
+                	//ADD FUNCTIONALITY TO API
+            }
+            catch (IOException e) 
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
