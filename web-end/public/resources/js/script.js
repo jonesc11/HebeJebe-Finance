56e0e34c81febdb69a69cd7a4c7f0180748805c3
box <jonesc11@rpi.edu>
2018-03-25 23:57:36 +0000

    Buggy add transaction code

--- web-end/public/resources/js/script.js
+++ web-end/public/resources/js/script.js
@@ -1,39 +1,6 @@
-$(document).ready (function () {
-	var app = angular.module("MyApp", []);
+var app = angular.module("MyApp", []);
 
-	app.controller = ("NewPanel", function($scope){
-		$scope.setTransactionType = function(type){
-			$scope.transactionType = type;
-		};
-
-		$scope.createTransaction = function(){
-		$http({
-  			method: 'POST',
-  			url: 'request/create/transactions',
-  			data: {
-  				"Limit": 30,
-				"TransactionType": $scope.transactionType, 
-  				"Amount": $scope.amount,
-  				"To": {"S": "<string>", "I": "<string>"},
-  				"From": {"S": "<string>", "I": "<string>"},
-  				"Description": "<string>"
-  				"DateTime": <datetime>,
-  				"Category": "<string>",
-  				"AssociatedWith": "<string>",
-  				"Recurring": $scope.recurring,
-  				"RecurringUntil": <datetime>,
-  				"RecurringFrequency": <dayinterval>	
-			      }
-			}).
-			then(function(success) {
-				$scope.transactions = data;
-			}).
-			then(function(error) {
-				// log error
-		});
-
-		};
-	});
+$(document).ready (function (app) {
 
 	app.controller("PostsCtrl", function($scope, $http) {
 		$http({
@@ -44,10 +11,43 @@ $(document).ready (function () {
 			      }
 			}).
 			then(function(success) {
-				$scope.transactions = data;
+				$scope.transactions = success.data;
 			}).
 			then(function(error) {
 				// log error
 		});
-	});  
-}());
+	}); 
+
+	app.controller("NewPanel", function($scope, $http) {
+
+		$scope.createTransaction = function(){
+	    	 $http({
+  			method: 'POST',
+  			url: 'request/create/transactions',
+  			data: {
+  				"Limit": 30,
+				"TransactionType": $scope.transactionType, 
+  				"Amount": $scope.amount,
+  				"To": {"S": "<string>", "I": "<string>"},
+  				"From": {"S": "<string>", "I": "<string>"},
+  				"Description": "<string>",
+  				"DateTime": "<datetime>",
+  				"Category": "<string>",
+  				"AssociatedWith": "<string>",
+  				"Recurring": $scope.recurring,
+  				"RecurringUntil": "<datetime>",
+  				"RecurringFrequency": "<dayinterval>",
+			      }
+			}).
+			then(function(success) {
+
+			}).
+			then(function(error) {
+				// log error
+		});
+
+		};
+	});
+
+	 
+}(app));
