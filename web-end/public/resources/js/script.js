562642064c24c09ebe89a8e73028bfc98b8858de
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-03-25 18:16:34 -0400

    Created angular to create transaction

--- web-end/public/resources/js/script.js
+++ web-end/public/resources/js/script.js
@@ -1,10 +1,44 @@
 $(document).ready (function () {
 	var app = angular.module("MyApp", []);
 
+	app.controller = ("NewPanel", function($scope){
+		$scope.setTransactionType = function(type){
+			$scope.transactionType = type;
+		};
+
+		$scope.createTransaction = function(){
+		$http({
+  			method: 'POST',
+  			url: 'request/create/transactions',
+  			data: {
+  				"Limit": 30,
+				"TransactionType": "Income|Expense|Transfer",
+  				"Amount": <float>,
+  				"To": {"S": "<string>", "I": "<string>"},
+  				"From": {"S": "<string>", "I": "<string>"},
+  				"Description": "<string>"
+  				"DateTime": <datetime>,
+  				"Category": "<string>",
+  				"AssociatedWith": "<string>",
+  				"Recurring": <boolean>,
+  				"RecurringUntil": <datetime>,
+  				"RecurringFrequency": <dayinterval>	
+			      }
+			}).
+			then(function(success) {
+				$scope.transactions = data;
+			}).
+			then(function(error) {
+				// log error
+		});
+
+		};
+	});
+
 	app.controller("PostsCtrl", function($scope, $http) {
 		$http({
   			method: 'GET',
-  			url: 'request/transactions',
+  			url: 'request/get/transactions',
   			data: {
   				"Limit": 30,
 			      }
