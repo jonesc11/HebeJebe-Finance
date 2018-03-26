faba118b661aa2254bb426c631e81ea600de9be4
hannahedeen <hannahedeen@gmail.com>
2018-03-25 20:10:02 -0400

    change to js client

--- src/finance/Message.java
+++
@@ -1,8 +0,0 @@
-
-public class String Message {
-
-    public String Message(String json){
-    		String msg = runClient(json);
-    		return msg;
-    }
-}
--- src/finance/Runner.java
+++ src/finance/Runner.java
@@ -11,21 +11,6 @@ Main function for initializing the server client and site.
 public class Runner {
 
     public static void main(String[] args) throws IOException{
-        
-        ObjectMapper mapper = new ObjectMapper();
-        File from = new File("config.txt");
-        TypeReference<HashMap<Integer,String>> typeRef
-                = new TypeReference<HashMap<Integer,String>>() {};
-
-        HashMap<Integer,String> ips = mapper.readValue(from, typeRef);
-
-        File f = new File("database.json");
-        if(f.exists() && !f.isDirectory()) {
-            System.out.println("file already exists, recovering data!");
-            ObjectMapper siteMapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        }else{
-        }
 
         startServer();
     }
