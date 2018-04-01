c48ba2a3859c19634f526f4f39257752d15e6615
hannahedeen <hannahedeen@gmail.com>
2018-04-01 16:59:48 -0400

    JUnit for account.java

---
+++ src/TestSuite/AccountTest.java
@@ -0,0 +1,27 @@
+package TestSuite;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+import finance.Account;
+
+public class AccountTest {
+	
+	private String name = "Hannah";
+	private String type = "Trust";
+	private double balance = 10000;
+
+	private Account correct = new Account(name, type, balance);
+	private Account correct2 = new Account("Collin", "Checking", balance);
+	private Account correct3 = new Account("Elizabeth", "Savings", balance);
+	
+	@Test
+	public void testGetters() {
+		assertEquals(name, correct.getName());
+		assertEquals(balance, correct.getBalance());
+		assertEquals(type, correct.getType());
+	}
+
+}
