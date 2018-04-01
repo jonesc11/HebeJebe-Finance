1dea51d371db6bec4b45047e10ba09cdffbfdd1f
hannahedeen <hannahedeen@gmail.com>
2018-04-01 15:36:22 -0400

    first junit test

---
+++ src/TestSuite/AllTests.java
@@ -0,0 +1,14 @@
+package TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	SingleExpenseTest.class
+	
+})
+public class AllTests {
+
+}
