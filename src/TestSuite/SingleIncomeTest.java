d3698a02cfdc0a518213ce44071b9c60aff8183f
hannahedeen <hannahedeen@gmail.com>
2018-04-05 16:25:06 -0400

    singleIncomeTest

---
+++ src/TestSuite/SingleIncomeTest.java
@@ -0,0 +1,28 @@
+package TestSuite;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import finance.Date;
+import finance.SingleIncome;
+
+public class SingleIncomeTest {
+	private double a = 300.00;
+	private String n = "yard work for gram";
+	private String c = "Other";
+	private Date d = new Date(30, 3, 2018);
+	private SingleIncome original = new SingleIncome(a, n, c, d);
+	
+	
+	@Test
+	public void testCreation() {
+		assertTrue(a == original.getAmount());
+		assertEquals("fail on getName() on singleIncome",n, original.getName());
+		assertEquals("fail on getCategorey() on singleIncome", c, original.getCategory());
+		assertEquals("fail on getDate() on singleIncome", d, original.getDate() );
+	}
+
+}
