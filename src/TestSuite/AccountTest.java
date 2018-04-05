e007be3ef805a01777c385f801dda6441d3faa8d
hannahedeen <hannahedeen@gmail.com>
2018-04-05 16:22:48 -0400

    adding for rid

--- src/TestSuite/AccountTest.java
+++ src/TestSuite/AccountTest.java
@@ -6,6 +6,10 @@ import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 import finance.Account;
+import finance.Runner;
+import finance.SingleExpense;
+import finance.Date;
+
 
 public class AccountTest {
 	
@@ -28,8 +32,17 @@ public class AccountTest {
 	public void resourceIdentifierItems() {
 	
 		correct.setResourceIdentifier("test1");
-		assertEquals("test1", correct.getResourceIdentifier());	
+		assertEquals("test1", correct.getResourceIdentifier());		
+	}
+	
+	@Test
+	public void addSingleExpenseTest() {
+	
+		correct2.addSingleExpense(20, "turtle", "test", new Date(5, 4, 2018) );
 		
+		assertTrue(correct2.getBalance() == 9980);
+		
+				
 	}
 
 }
