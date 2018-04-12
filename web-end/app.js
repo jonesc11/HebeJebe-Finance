d870e0e42bce42d8f255b7a2a595b12a121856d7
Collin Jones <jonesc11@rpi.edu>
2018-04-11 20:55:55 -0400

    cdj - syntax err

--- web-end/app.js
+++ web-end/app.js
@@ -20,7 +20,7 @@ app.listen (80);
 
 app.use (express.static (__dirname + '/public'));
 app.use (bodyParser.json());
-app.use (bodyParser.urlencoded({ extended: true });
+app.use (bodyParser.urlencoded({ extended: true }));
 app.use (cookieParser());
 
 app.get ('/', function (req, res) {
