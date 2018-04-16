07f09defff98c6599cda376df0e60f16d97b2b7b
elizabethdinella <elizabeth.a.dinella@gmail.com>
2018-04-16 18:11:05 +0000

    Reorganized controllers & created routes

--- web-end/public/resources/js/script.js
+++ web-end/public/resources/js/script.js
@@ -2,12 +2,23 @@ var app = angular.module("MyApp", ['ngRoute']);
 
 $(document).ready (function (app) {
 
-	app.controller("LoginCtrl", function($scope, $http){
+
+	app.config(["$routeProvider", function($routeProvider){
+		$routeProvider
+		.when("/", {
+			controller: 'HomeCtrl',
+			templateUrl: '/resources/views/home.html'
+		})
+		.otherwise({redirectTo: "/"})
+
+	}]);
+
+	app.controller("NavBarCtrl", function($rootScope, $scope, $http){
 
 	});
 
-
-	app.controller("SignupCtrl", function($scope, $http){
+	
+	app.controller("HomeCtrl", function($rootScope, $scope, $http){
 		$scope.page1 = true;
 		$scope.page2 = false;
 
@@ -37,9 +48,8 @@ $(document).ready (function (app) {
 			})
 		};
 
-	});
 
-	app.controller("PostsCtrl", function($scope, $http) {
+
 		$http({
   			method: 'GET',
   			url: '/request/get/transactions',
@@ -53,25 +63,24 @@ $(document).ready (function (app) {
 			then(function(error) {
 				// log error
 		});
-	}); 
 
-	app.controller("NewPanel", function($scope, $http) {
-        var getAccounts = function () {
-            $http({
-                method: 'GET',
-                url: '/request/get/accounts',
-                data: {
-                    "Limit": 30
-                    }
-                }).then(function(success) {
-                    $scope.accounts = success.data.Account;
-                }).
-                then(function(error) {
-                    // log error
-            });
-        };
+
+        	var getAccounts = function () {
+            		$http({
+                		method: 'GET',
+                		url: '/request/get/accounts',
+                		data: {
+                    			"Limit": 30
+                    		}
+                	}).then(function(success) {
+                    		$scope.accounts = success.data.Account;
+                	}).
+                	then(function(error) {
+                    		// log error
+            		});
+        	};
         
-        getAccounts();
+        	getAccounts();
 
 		$scope.createTransaction = function(){
 	    	 $http({
@@ -92,32 +101,32 @@ $(document).ready (function (app) {
   				"RecurringFrequency": $scope.transactionRecurInterval,
 			      }
 			}).
-			then(function(success) {
+				then(function(success) {
 			}).
-			then(function(error) {
+				then(function(error) {
 				// log error
-		});
+			});
 
 		};
         
-        $scope.createAccount = function () {
-            $http ({
-                method: 'POST',
-                url: '/request/create/account',
-                data: {
-                    AccountName: $scope.createAccountName,
-                    AccountBalance: $scope.createAccountBalance,
-                    AccountType: $scope.createAccountType
-                }
-            }).then (function (success) {
-                console.log (success.data);
-                getAccounts();
-                $("#createAccountModal").modal('toggle');
-                $("input[name=create-account-name]").val("");
-                $("input[name=create-account-balance]").val("");
-            }).then (function (error) {
-                // log error
-            });
+        	$scope.createAccount = function () {
+            	$http ({
+                	method: 'POST',
+                	url: '/request/create/account',
+                	data: {
+                    		AccountName: $scope.createAccountName,
+                    		AccountBalance: $scope.createAccountBalance,
+                    		AccountType: $scope.createAccountType
+                	}
+            	}).then (function (success) {
+                	console.log (success.data);
+                	getAccounts();
+                	$("#createAccountModal").modal('toggle');
+                	$("input[name=create-account-name]").val("");
+                	$("input[name=create-account-balance]").val("");
+            	}).then (function (error) {
+                	// log error
+            	});
         };
 	});
 	 
